dcbf161b4004b9eca9638af55a59c1eeed8f7ad7
unknown <kamil.kaczor1234@gmail.com>
2020-08-27 13:46:05 +0200

    api http client created

---
+++ app/Http/Clients/BitBayHttpClient.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Clients;
+
+use GuzzleHttp\Client;
+
+class BitBayHttpClient
+{
+    private string $url;
+    private Client $client;
+
+    public function __construct(Client $client)
+    {
+        $this->url = env('BITBAY_API_URL');
+
+        $this->client = $client;
+    }
+
+    public function getBitcoinTradeHistory(): ?object
+    {
+        $response = $this->client->request('GET', $this->url);
+
+        if(!($body = $response->getBody())){
+            return null;
+        }
+
+        return json_decode($body->getContents());
+    }
+
+}
